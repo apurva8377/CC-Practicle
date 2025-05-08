832d78d2b50d254d98e9186c96e34966524569a9
apurva8377 <apurvashinde098@gmail.com>
2025-05-08 22:36:07 +0530

    Add files via upload

---
+++ Apurva1.py
@@ -0,0 +1,11 @@
+# Subtract two numbers
+
+# Input numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Subtract
+result = num1 - num2
+
+# Output result
+print("The result of subtraction is:", result)
