a44fecc5f487373be7262ade7b5009f78d15d606
apurva8377 <apurvashinde098@gmail.com>
2025-05-08 23:10:50 +0530

    Update Apurva1.py

--- Apurva1.py
+++ Apurva1.py
@@ -1,11 +1,1 @@
-# Subtract two numbers
-
-# Input numbers
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-
-# Subtract
-result = num1 - num2
-
-# Output result
-print("The result of subtraction is:", result)
+print("Hello World")
